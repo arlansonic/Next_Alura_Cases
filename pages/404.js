4fedd94388e0ff987ac82df9c124c03b473b9a8d
Arlan Marreiro <arlan_marreiro@cal-comp.com.br>
2022-07-27 22:45:54 -0400

    Adicionando tela 404 e inserindo CSS

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Link from '../src/components/Link'
+
+export default function Page404() {
+    return (
+        <div>
+            <h1>PAGINA 404!!</h1>
+            <Link href="/">
+                Ir para o Home
+            </Link>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,28 @@
 import Link from '../src/components/Link'
+import React from 'react'
+
+
+function Title({ children, as }) {
+    const Tag = as
+    return (
+        <React.Fragment>
+            <Tag>
+                {children}
+            </Tag>
+            <style jsx>{`
+                ${Tag} {
+                    color: red;
+                }
+            `}
+            </style>
+        </React.Fragment>
+    )
+}
 
 export default function HomePage() {
     return (
         <div>
-            <h1>Alura Cases - Home</h1>
+            <Title as="h1">Alura Cases - Home</Title>
             <Link href="/faq">
                 Ir para a Pagina do FAQ
             </Link>
