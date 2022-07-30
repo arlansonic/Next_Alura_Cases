82e542e38c1734f3be9f330d8d6059b055079d8c
Arlan Marreiro <arlan_marreiro@cal-comp.com.br>
2022-07-29 20:54:04 -0400

    Finalizando Projeto

--- pages/faq.js
+++ pages/faq.js
@@ -1,12 +1,48 @@
+import Head from 'next/head'
 import Link from '../src/components/Link'
 
-export default function FaqPage() {
+export async function getServerSideProps() {
+
+    const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
+    const faq = await fetch(FAQ_API_URL)
+        .then((response) => {
+            return response.json()
+        })
+        .then((resp) => {
+            return resp
+        })
+
+    return {
+        props: {
+            QualquerCoisa: 'Vai passar aqui',
+            faq
+        },
+    }
+}
+
+export default function FaqPage({ faq }) {
+
+    console.log(faq)
+
     return (
         <div>
+            <Head>
+                <title>FAQ - Alura Cases Campanha</title>
+            </Head>
             <h1>Alura Cases - FAQ</h1>
             <Link href="/">
                 PAG INÍCIO
             </Link>
-        </div>
+            <ul>
+                {faq.map(({ answer, question }) => (
+                    <li key={question}>
+                        <article>
+                            <h2>{question}</h2>
+                            <p>{answer}</p>
+                        </article>
+                    </li>
+                ))}
+            </ul>
+        </div >
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,38 @@
-import Link from '../src/components/Link'
-import React from 'react'
+import HomeScreen from "../src/screens/HomeScreen";
+
+export default HomeScreen
+
+// import Link from '../src/components/Link'
+// import React from 'react'
+// import Footer from '../src/components/patterns/Footer'
 
 
-function Title({ children, as }) {
-    const Tag = as
-    return (
-        <React.Fragment>
-            <Tag>
-                {children}
-            </Tag>
-            <style jsx>{`
-                ${Tag} {
-                    color: red;
-                    font-family: sans-serif;
-                }
-            `}
-            </style>
-        </React.Fragment>
-    )
-}
+// function Title({ children, as }) {
+//     const Tag = as
+//     return (
+//         <React.Fragment>
+//             <Tag>
+//                 {children}
+//             </Tag>
+//             <style jsx>{`
+//                 ${Tag} {
+//                     color: red;
+//                     font-family: sans-serif;
+//                 }
+//             `}
+//             </style>
+//         </React.Fragment>
+//     )
+// }
 
-export default function HomePage() {
-    return (
-        <div>
-            <Title as="h1">Alura Cases - Home</Title>
-            <Link href="/faq">
-                Ir para a Pagina do FAQ
-            </Link>
-        </div>
-    )
-}
+// export default function HomePage() {
+//     return (
+//         <div>
+//             <Title as="h1">Alura Cases - Home</Title>
+//             <Link href="/faq">
+//                 Ir para a Pagina do FAQ
+//             </Link>
+//             <Footer/>
+//         </div>
+//     )
+// }
